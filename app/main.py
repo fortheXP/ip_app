ad44f08fd6edfe7708dd4427c1b05ed98801c436
forthexp <bharathsma1lnowout@outlook.com>
2026-05-13 23:38:28 +0530

    format code and remove pytest from requirements

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.get("/")
 def get_ip():
     client_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
@@ -10,9 +11,11 @@ def get_ip():
         client_ip = client_ip.split(",")[0].strip()
     return jsonify(your_ip=client_ip)
 
+
 @app.get("/health")
 def health():
     return jsonify(status="healthy")
+
 
 @app.get("/ready")
 def ready():
